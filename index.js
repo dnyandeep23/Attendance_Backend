069a9d7e9a842b800d160e6687e649b461e65c26
ITS-DNY <104913044+ITS-DNY@users.noreply.github.com>
2023-10-19 10:18:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,66 @@
-// import { initializeApp, credential as _credential } from "firebase-admin";
 import { initializeApp, applicationDefault } from 'firebase-admin/app';
-import { getMessaging } from 'firebase-admin/messaging';
-import express, { json } from 'express';
-import cors from 'cors';
-// import serviceAccount from "path/to/serviceAccountKey.json";
+import { getMessaging } from "firebase-admin/messaging";
+import express, { json } from "express";
+import cors from "cors";
+
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 const app = express();
 app.use(express.json());
 
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
-app.use(cors({
-    origin: "*",
-}));
-
-app.use(cors({
-    methods: ["GET", "POST", "PUT", "DELETE", "UPDATE", "PATCH"],
-}));
-
+app.use(
+    cors({
+        methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    })
+);
 
 app.use(function (req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     next();
 });
 
+
 initializeApp({
     credential: applicationDefault(),
-    databaseURL: "https://attendance-go-default-rtdb.firebaseio.com"
+    projectId: 'potion-for-creators',
 });
-
-
 
 app.post("/send", function (req, res) {
     const receivedToken = req.body.fcmToken;
+
     const message = {
         notification: {
-            title: "Notification",
-            body: "This is a notification"
+            title: "Notif",
+            body: 'This is a Test Notification'
         },
-        token: "ejW6Y1ECT0qcq5wmOKVCpz:APA91bEIszdL-ho1PybyAYTZvYwCvLKRi_sO-kD7XsIVBeIbo14IemB0ARzhP4nS9sHdLY87e8B6CmeMhh4ZET39f4SE8EEjuQYqNCThU8hYdp7DUueEPn7AuicQmxdbp8KPGY2dv1-y"
-    }
-    getMessaging().send(message).then((response) => {
-        res.status(200).json({
-            message: "Successfully sent message",
-            token: receivedToken
-        });
-        console.log(`Successfully message sent ${response}`);
+        token: "ejW6Y1ECT0qcq5wmOKVCpz:APA91bEIszdL-ho1PybyAYTZvYwCvLKRi_sO-kD7XsIVBeIbo14IemB0ARzhP4nS9sHdLY87e8B6CmeMhh4ZET39f4SE8EEjuQYqNCThU8hYdp7DUueEPn7AuicQmxdbp8KPGY2dv1-y",
+    };
 
-    }).catch((error) => {
-        res.status(400);
-        res.send(error);
-        console.log("Error: " + error);
-    })
+    getMessaging()
+        .send(message)
+        .then((response) => {
+            res.status(200).json({
+                message: "Successfully sent message",
+                token: receivedToken,
+            });
+            console.log("Successfully sent message:", response);
+        })
+        .catch((error) => {
+            res.status(400);
+            res.send(error);
+            console.log("Error sending message:", error);
+        });
+
+
 });
 
 app.listen(3000, function () {
-    console.log("Server is listening on 3000");
+    console.log("Server started on port 3000");
 });
